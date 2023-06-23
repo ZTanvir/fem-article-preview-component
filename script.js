365b802e277d69d0215565c822433e0979dad43b
ztanvir <zahirulopel@gmail.com>
2023-06-23 17:06:23 +0600

    Add share section for mobile design done

--- script.js
+++ script.js
@@ -5,4 +5,20 @@ const desktopShareEl = document.querySelector(".article__share-desktop");
 shareDestopBtn.addEventListener("click", (e) => {
   desktopShareEl.classList.toggle("active-desktop-share");
 });
+
+const articleShareMobileEl = document.querySelector(".article__share-mobile");
+const mobileShareImg = document.querySelector(".mobile__share-img");
+const authorInfoEl = document.querySelector(".article__mobile_author-info");
+const shareIconsEl = document.querySelector(".share-mobile__icon");
+
 //
+mobileShareImg.addEventListener("click", (e) => {
+  authorInfoEl.classList.toggle("hide-info-mobile");
+  shareIconsEl.classList.toggle("hide-info-mobile");
+  if (authorInfoEl.classList.contains("hide-info-mobile")) {
+    articleShareMobileEl.style.backgroundColor = "var(--cc-dark-grayish-blue)";
+  } else {
+    articleShareMobileEl.style.backgroundColor = "white";
+  }
+  console.log(articleShareMobileEl, mobileShareImg, authorInfoEl, shareIconsEl);
+});
