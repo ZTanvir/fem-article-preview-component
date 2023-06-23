d78706a43f3d428ef1417d2818f9799a07048123
ztanvir <zahirulopel@gmail.com>
2023-06-23 15:48:43 +0600

    Add js comment to identify which code do what

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const shareDestopBtn = document.querySelector(".share-arrow-destop");
 const desktopShareEl = document.querySelector(".article__share-desktop");
 
+// Show social share section by click on share icon for desktops
 shareDestopBtn.addEventListener("click", (e) => {
   desktopShareEl.classList.toggle("active-desktop-share");
 });
+//
